a1bd2fb08d5a5729ac71a32e4c3bfee427b43563
justinsullivan2004 <sul22007@byui.edu>
2025-02-26 09:45:41 -0700

    changes

--- courses.js
+++ courses.js
@@ -1,5 +1,7 @@
 import {aCourse} from "./course.mjs";
 
+aCourse.enrollStudent();
+
   document.querySelector("#enrollStudent").addEventListener("click", function () {
     const sectionNum = document.querySelector("#sectionNumber").value;
     aCourse.enrollStudent(sectionNum);
